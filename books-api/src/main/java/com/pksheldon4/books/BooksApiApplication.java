2f20fdd5692ede6585d5f0a7479c5d8fed7dbdf0
Preston Sheldon <psheldon@pivotal.io>
2019-07-12 11:18:04 -0400

    Updates to support running on google cloud

--- books-api/src/main/java/com/pksheldon4/books/BooksApiApplication.java
+++ books-api/src/main/java/com/pksheldon4/books/BooksApiApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleSpringExamplesApplication {
+public class BooksApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleSpringExamplesApplication.class, args);
+		SpringApplication.run(BooksApiApplication.class, args);
 	}
 }
